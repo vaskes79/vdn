c6d603a3bc751552b8b67dd1611ae99f60e173c9
Vasily Guzov <vasily.guzov@gmail.com>
2020-07-18 15:56:23 +0300

    return material ui theme provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import VdnApp from 'components/VdnApp';
 
-ReactDOM.render(<VdnApp />, document.getElementById('root'));
+const theme = createMuiTheme();
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <VdnApp />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
