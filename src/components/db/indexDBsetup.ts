5282fb8316606e8e174f73a1b675191b89117a07
Vasily Guzov <vasily.guzov@gmail.com>
2020-09-12 23:17:10 +0300

    create interfaces for DBsetup

--- src/components/db/indexDBsetup.ts
+++ src/components/db/indexDBsetup.ts
@@ -1,7 +1,56 @@
 import { openDB } from 'idb';
 import { AppSetup } from 'components/constants';
 
-const setupDB = async ({ settingsStore, demoVideo }) => {
+interface NoteFormat {
+  name: string;
+  value: boolean;
+  label: string;
+  description: string;
+}
+
+interface TimeOffset {
+  name: string;
+  value: number;
+  label: string;
+  description: string;
+}
+
+interface CurrentVideo {
+  name: string;
+  value: string;
+  label: string;
+  description: string;
+}
+
+interface Note {
+  url: string;
+  title: string;
+  time: number;
+}
+
+interface DemoVideo {
+  url: string;
+  title: string;
+  notes: Array<Note>;
+}
+
+interface SettingsStore {
+  html: NoteFormat;
+  md: NoteFormat;
+  txt: NoteFormat;
+  current_video: CurrentVideo;
+  time_offset: TimeOffset;
+}
+
+interface Config {
+  settingsStore: SettingsStore;
+  demoVideo: DemoVideo;
+}
+
+
+const setupDB = async (config: Config) => {
+
+  const {settingsStore, demoVideo} = config;
 
   let db = await openDB(AppSetup.NAME, AppSetup.VER, {
     upgrade(db) {
